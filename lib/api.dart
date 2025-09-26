e96e63b3c131e9303a357e1cb7291c6ad98b29e3
MajaBrorsson <gusbromaap@student.gu.se>
2025-09-26 11:58:00 +0200

    Fixa kommentarer och felhantering i api filen

--- lib/api.dart
+++ lib/api.dart
@@ -11,49 +11,62 @@ Future<String> registerApiKey() async {
   if (response.statusCode == 200) {
     return response.body;
   } else {
+    // Om statuskoden inte är lyckad visas felmeddelande
     throw Exception('Failed to get API key');
   }
 }
 
-// Hämta lista av todos från API och retunerar en omvandlad lista av ToDoApi-objekt
+// Hämta lista av todos från API
 Future<List<ToDo>> getToDo() async {
   http.Response response = await http.get(
     Uri.parse('$ENDPOINT/todos?key=$apikey'),
   );
-  String body = response.body;
-  print("alla todos: ${response.body}");
-  // Gör om JSON-strängen till en lista av Map-objekt
-  List<dynamic> jsonList = jsonDecode(body);
 
-  // Omvandla varje JSON-objekt i jsonList till ett ToDoApi-objekt med factory,
+  // Om statuskoden inte är mellan 200–299 (lyckad) visas felmeddelande
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    throw Exception('Failed to fetch todos');
+  }
+
+  // Gör om JSON-strängen till en lista av Map-objekt
+  List<dynamic> jsonList = jsonDecode(response.body);
+
+  // Omvandla varje JSON-objekt i jsonList till ett ToDo-objekt med factory,
   // och samla alla objekt i en lista
   List<ToDo> todos = jsonList.map((json) => ToDo.fromJson(json)).toList();
 
   return todos;
 }
 
-// Skickar ny todo till apin
+// Skicka ny todo till API
 Future<void> addToDo(ToDo todo) async {
   http.Response response = await http.post(
     Uri.parse('$ENDPOINT/todos?key=$apikey'),
     headers: {"Content-Type": "application/json"},
     body: jsonEncode(todo.toJson()),
-  );
-  print("Adderat: ${response.body}");
+  ); // Om statuskoden inte är mellan 200–299 visas felmeddelande
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    throw Exception('Failed to add todo');
+  }
 }
 
+// Uppdatera todo i API
 Future<void> updateToDo(ToDo todo) async {
   http.Response response = await http.put(
     Uri.parse('$ENDPOINT/todos/${todo.id}?key=$apikey'),
     headers: {"Content-Type": "application/json"},
     body: jsonEncode(todo.toJson()),
-  );
-  print("uppdaterat: ${response.body}");
+  ); // Om statuskoden inte är mellan 200–299 visas felmeddelande
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    throw Exception('Failed to update todo');
+  }
 }
 
+// Radera todo i API
 Future<void> deleteTodo(ToDo todo) async {
   http.Response response = await http.delete(
     Uri.parse('$ENDPOINT/todos/${todo.id}?key=$apikey'),
-  );
-  print("raderat: ${todo}");
+  ); // Om statuskoden inte är mellan 200–299 visas felmeddelande
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    throw Exception('Failed to delete todo');
+  }
 }
